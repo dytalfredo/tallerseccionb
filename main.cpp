c64eb95634c3b0066c8387a6617f1638bbba2994
dytalfredo <121588861+dytalfredo@users.noreply.github.com>
2024-06-28 10:09:50 -0400

    Update post class main.cpp

--- main.cpp
+++ main.cpp
@@ -17,15 +17,20 @@ int main(int argc, char** argv) {
 	int nuevocliente=1;
 	int cantidadrefresco=0;
 	int cantidadmalta=0;
+    int cantidadclientes=0;
+    int cantidadcompras=0;
 	
 	while (nuevocliente==1) {
+    
+    cantidadclientes=cantidadclientes+1;
+        monto_cliente=0;
 		std::cout<<"ingrese razon social"<< std::endl;
 		std::cin>> razonsocial;
 		std::cout<<"ingrese cedula o rif"<< std::endl;
 		std::cin>> rif_cedula;
 		std::cout<<"¿cuantos refresco compro?"<< std::endl;
 		std::cin>> cantidadrefresco;
-
+cantidadcompras=cantidadcompras+1;
 
 
 		if (cantidadrefresco>=6) {
@@ -40,6 +45,7 @@ int main(int argc, char** argv) {
 		
 		std::cout<<"¿cuantas maltas compro?"<< std::endl;
 		std::cin>> cantidadmalta;
+        cantidadcompras=cantidadcompras+1;
 		if (cantidadmalta>=6) {
 			monto_cliente=monto_cliente+(cantidadmalta*maltamayor);
 			comprasmayor=comprasmayor+1;
@@ -49,8 +55,9 @@ int main(int argc, char** argv) {
 			comprasdetal=comprasdetal+1;
 			
 		}
+        totalcompra=totalcompra + monto_cliente;
 		
-		std::cout<<"el monto a pagar es: "<< monto_cliente<<std::endl;
+		std::cout<<"El Monto a pagar por " <<razonsocial <<" es : "<< monto_cliente <<"$"<<std::endl;
 		std::cout<<"¿desea ingresar otro cliente? presione 1 para si, o 2 para no"<< std::endl;
 		std::cin>> nuevocliente;
 		
@@ -58,6 +65,9 @@ int main(int argc, char** argv) {
 	
 	std::cout<<"Cantidad compras al Mayor: "<< comprasmayor<<std::endl;
 	std::cout<<"Cantidad compras al Detal: "<< comprasdetal<<std::endl;
+    std::cout<<"Porcentaje compras al Mayor: "<< comprasmayor*100/cantidadcompras <<"%"<<std::endl;
+	std::cout<<"Porcentaje compras al Menor: "<< comprasdetal*100/cantidadcompras <<"%"<<std::endl;
+	std::cout<<"Total en $ de Ventas con" <<cantidadclientes <<"clientes es: "<< totalcompra<<"$"<<std::endl;
 	
 	
 	return 0;
